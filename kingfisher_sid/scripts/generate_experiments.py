ffe634797bd10e7062a433d08bec39c3ff12c1c8
Luis Felipe Wolf Batista <luis.batista@gatech.edu>
2024-02-17 18:25:11 +0100

    Update experiements based on field tests
    Battery can hold long enough and there is no need for alternating
    experiment types based on priority.
    
    The circle experiment need longer time for completing full turn.

--- kingfisher_sid/scripts/generate_experiments.py
+++ kingfisher_sid/scripts/generate_experiments.py
@@ -9,12 +9,9 @@ main_duration = 20.0
 cool_down_duration = 10.0
 import rospkg
 
+# ACCERATION EXPERIMENTS
 
-#####################################################
-# ACCERATION EXPERIMENTS HIGH PRIORITY
-#####################################################
-
-accelerations = [1.0, 0.8, 0.6, 0.4]
+accelerations = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5]
 for acc in accelerations:
     section = {
         'experiment_name': f'acceleration_{acc}',
@@ -27,9 +24,10 @@ for acc in accelerations:
     }
     experiments.append(section)
 
-# ROTATION EXPERIMENTS HIGH PRIORITY
-accelerations_right = [1.0, 0.8, 0.6]
-accelerations_left = [0.5762, 0.5190, 0.4529]
+# ROTATION EXPERIMENTS
+accelerations_right = [1.0, 0.9, 0.8, 0.7, 0.6]
+accelerations_left = [0.5762, 0.5456, 0.5190, 0.4857, 0.4529]
+
 for accl, accr in zip(accelerations_left, accelerations_right):
     section = {
         'experiment_name': f'rotation_{accr}',
@@ -43,33 +41,36 @@ for accl, accr in zip(accelerations_left, accelerations_right):
     experiments.append(section)
 
     
-# CIRCLE EXPERIMENTS HIGH PRIORITY
-accelerations = [1.0]
+# CIRCLE EXPERIMENTS
+circle_duration = 40
+accelerations = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5]
 for acc in accelerations:
     section = {
         'experiment_name': f'circle_left_{acc}',
         'sections': [
             { 'duration': late_start_duration,  'tl': 0.0, 'tr': 0.0 },
             { 'duration': warmup_duration,      'tl': 0.0, 'tr': acc },
-            { 'duration': main_duration,        'tl': 0.0, 'tr': acc },
+            { 'duration': circle_duration,        'tl': 0.0, 'tr': acc },
             { 'duration': cool_down_duration,   'tl': 0.0, 'tr': 0.0 }
         ]
     }
     experiments.append(section)
-
+for acc in accelerations:
     section = {
         'experiment_name': f'circle_right_{acc}',
         'sections': [
             { 'duration': late_start_duration,  'tl': 0.0, 'tr': 0.0 },
             { 'duration': warmup_duration,      'tl': acc, 'tr': 0.0 },
-            { 'duration': main_duration,        'tl': acc, 'tr': 0.0 },
+            { 'duration': circle_duration,        'tl': acc, 'tr': 0.0 },
             { 'duration': cool_down_duration,   'tl': 0.0, 'tr': 0.0 }
         ]
     }
     experiments.append(section)
 
+
+# ZIGZAG EXPERIMENTS
 zigzag_duration = 3.0
-accelerations = [1.0]
+accelerations = [1.0, 0.8]
 for acc in accelerations:
     section = {
         'experiment_name': f'zigzag_{acc}',
@@ -89,65 +90,6 @@ for acc in accelerations:
     }
     experiments.append(section)
 
-
-#####################################################
-# low priority experiments
-#####################################################
-    
-# ACCERATION EXPERIMENTS LOW PRIORITY
-accelerations = [0.9, 0.7, 0.5]
-for acc in accelerations:
-    section = {
-        'experiment_name': f'acceleration_{acc}',
-        'sections': [
-            { 'duration': late_start_duration,  'tl': 0.0, 'tr': 0.0 },
-            { 'duration': warmup_duration,      'tl': acc, 'tr': acc },
-            { 'duration': main_duration,        'tl': acc, 'tr': acc },
-            { 'duration': cool_down_duration,   'tl': 0.0, 'tr': 0.0 }
-        ]
-    }
-    experiments.append(section)
-
-# ROTATION EXPERIMENTS LOW PRIORITY
-accelerations_right = [0.9, 0.7]
-accelerations_left = [0.5456, 0.4857] # TODO fix the correct speed
-for accl, accr in zip(accelerations_left, accelerations_right):
-    section = {
-        'experiment_name': f'rotation_{accr}',
-        'sections': [
-            { 'duration': late_start_duration,  'tl': 0.0,   'tr': 0.0 },
-            { 'duration': warmup_duration,      'tl': accl, 'tr': -accr },
-            { 'duration': main_duration,        'tl': accl, 'tr': -accr },
-            { 'duration': cool_down_duration,   'tl': 0.0,   'tr': 0.0 }
-        ]
-    }
-    experiments.append(section)
-
-    
-# CIRCLE EXPERIMENTS LOW PRIORITY 
-accelerations = [0.8, 0.6]
-for acc in accelerations:
-    section = {
-        'experiment_name': f'circle_left_{acc}',
-        'sections': [
-            { 'duration': late_start_duration,  'tl': 0.0, 'tr': 0.0 },
-            { 'duration': warmup_duration,      'tl': 0.0, 'tr': acc },
-            { 'duration': main_duration,        'tl': 0.0, 'tr': acc },
-            { 'duration': cool_down_duration,   'tl': 0.0, 'tr': 0.0 }
-        ]
-    }
-    experiments.append(section)
-
-    section = {
-        'experiment_name': f'circle_right_{acc}',
-        'sections': [
-            { 'duration': late_start_duration,  'tl': 0.0, 'tr': 0.0 },
-            { 'duration': warmup_duration,      'tl': acc, 'tr': 0.0 },
-            { 'duration': main_duration,        'tl': acc, 'tr': 0.0 },
-            { 'duration': cool_down_duration,   'tl': 0.0, 'tr': 0.0 }
-        ]
-    }
-    experiments.append(section)
 
 
 #####################################################
@@ -168,30 +110,6 @@ for acc in accelerations:
             ]
     }
     experiments.append(section)
-
-
-# ZIGZAG EXPERIMENTS
-zigzag_duration = 3.0
-accelerations = [0.6]
-for acc in accelerations:
-    section = {
-        'experiment_name': f'zigzag_{acc}',
-        'sections': [
-                { 'duration': late_start_duration,    'tl': 0.0, 'tr': 0.0 },
-                { 'duration': warmup_duration,        'tl': acc, 'tr': acc },
-                { 'duration': zigzag_duration,        'tl': acc, 'tr': 0.0 },
-                { 'duration': zigzag_duration,        'tl': 0.0, 'tr': acc },
-                { 'duration': zigzag_duration,        'tl': acc, 'tr': 0.0 },
-                { 'duration': zigzag_duration,        'tl': 0.0, 'tr': acc },
-                { 'duration': zigzag_duration,        'tl': acc, 'tr': 0.0 },
-                { 'duration': zigzag_duration,        'tl': 0.0, 'tr': acc },
-                { 'duration': zigzag_duration,        'tl': acc, 'tr': 0.0 },
-                { 'duration': zigzag_duration,        'tl': 0.0, 'tr': acc },
-                { 'duration': cool_down_duration,     'tl': 0.0, 'tr': 0.0 }
-            ]
-    }
-    experiments.append(section)
-
 
 
 # Write the experiments to the yaml file
