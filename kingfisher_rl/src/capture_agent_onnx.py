3d2f6651e59bae5c39b6efd2b24872c1af1695c1
Luis Felipe Wolf Batista <luis.batista@gatech.edu>
2025-06-03 18:24:44 +0200

    Cleanup unused code and refactor observation buffers

--- kingfisher_rl/src/capture_agent_onnx.py
+++ kingfisher_rl/src/capture_agent_onnx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import rospy
-from geometry_msgs.msg import TwistStamped, PoseStamped, Twist, Vector3Stamped
+from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Bool, String
 from nav_msgs.msg import Odometry
 from heron_msgs.msg import Drive
@@ -9,7 +9,6 @@ import tf2_geometry_msgs
 import tf2_ros
 import rospkg
 
-import torch
 import onnxruntime as ort
 import numpy as np
 import math
@@ -17,10 +16,6 @@ import threading
 
 from visualization_msgs.msg import Marker
 
-# print path of the rl_games python library
-import rl_games
-import time
-print(rl_games.__file__)
 
 class RLAgent:
 
@@ -31,8 +26,6 @@ class RLAgent:
         self.tf_buffer = tf2_ros.Buffer(rospy.Duration(5.0))
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
-        # rl_config = rospy.get_param("~config_file", "test.yaml")
-        # rl_policy = rospy.get_param("~policy_file", "test.pth")
         model_path = rospy.get_param("~onnx_path", "test")
         # Get the file name of the model path  (without the path and extension)
         self.model_name = (f"{model_path}").replace(".", "_")
@@ -42,26 +35,11 @@ class RLAgent:
 
         model_folder = rospkg.RosPack().get_path('kingfisher_rl') + "/models/"
         print(f"Config folder: {model_folder}")
-        # self.rl_config_path = config_folder+rl_config
-        # self.rl_policy_path = config_folder+rl_policy
+
         self.rl_onnx_path = model_folder+model_path
         self.device = 'cpu'
 
-        num_obs = 8
-        obs_buffer_len = rospy.get_param("~obs_buffer_len", 1)
-        size_buffer = obs_buffer_len * num_obs
-
-        # with open(self.rl_config_path, 'r') as stream:
-        #     self.cfg = yaml.safe_load(stream)
-        
-        # observation_space = spaces.Dict({"state":spaces.Box(np.ones(size_buffer) * -np.Inf, np.ones(size_buffer) * np.Inf)})
-        # action_space = spaces.Box(low=np.array([-1.0, -1.0]), high=np.array([1.0, 1.0]), dtype=np.float64)
-        
-        # self.player = BasicPpoPlayerContinuous(self.cfg, observation_space, action_space, clip_actions=True, deterministic=True, device=self.device)
-        # self.player.restore(self.rl_policy_path)
-
         print(f"Loading model from {model_path}")
-        # self.onnx_model = onnx.load(self.rl_onnx_path)
         self.ort_model = ort.InferenceSession(self.rl_onnx_path, providers=['CPUExecutionProvider'])
 
         self.goal_robot = None
@@ -72,18 +50,18 @@ class RLAgent:
         goal_timeout_value = rospy.get_param("~goal_timeout", 5.0)
         self.goal_timeout = rospy.Duration(goal_timeout_value)
 
-        self.lin_vel = [0.0, 0.0]
-        self.ang_vel = [0.0]
-        self.heading = [0.0, 0.0]
-        self.robot_position = [0.0, 0.0]
-        self.previous_actions = [0.0, 0.0]
+        # Initialize np arrays for observations
+        self.prev_actions = np.zeros((1,2), dtype=np.float32)
+        self.lin_vel = np.zeros((1,2), dtype=np.float32)
+        self.ang_vel = np.zeros((1,1), dtype=np.float32)
+        self.goal_cos_sin = np.zeros((1,2), dtype=np.float32) # cos, sin
+        self.goal_distance = np.zeros((1,1), dtype=np.float32)
 
-        #buffers for observations
-        self._obs_buffer = torch.zeros((1, obs_buffer_len, num_obs), dtype=torch.float32)
-
-        # TODO: review topics that are hardcoded
+        # Subscribers
         rospy.Subscriber("~goal", PoseStamped, self.goal_cb, queue_size=1)
         rospy.Subscriber("~odom", Odometry, self.odom_cb, queue_size=1)
+
+        # Publishers
         self.cmd_drive_pub_ = rospy.Publisher("~cmd_drive", Drive, queue_size=1)
         self.rl_status_pub_ = rospy.Publisher("~rl_status", Bool, queue_size=1)
         self.marker_pub = rospy.Publisher("~goal_marker", Marker, queue_size = 1)
@@ -126,9 +104,8 @@ class RLAgent:
 
     def get_observations(self):
 
-        # convert to robot position from goal perspective with ofset
+        # Update and compute the values for the observations
         with self.odom_lock:
-            robot_pos = [self.odom.pose.pose.position.x, self.odom.pose.pose.position.y]
             robot_vel = [self.odom.twist.twist.linear.x, self.odom.twist.twist.linear.y]
             robot_ang_vel = [self.odom.twist.twist.angular.z]
         goal_pos = [self.goal_robot.pose.position.x, self.goal_robot.pose.position.y]
@@ -136,33 +113,26 @@ class RLAgent:
         goal_distance = min(np.linalg.norm(np.array(goal_pos)), 9.0) # Limit the distance to 9m
         goal_cos_sin = [math.cos(goal_bearing), math.sin(goal_bearing)]
 
-        # rospy.loginfo_throttle(1, f"OBS Position  \t{goal_pos[0]:.2f}, {goal_pos[1]:.2f}")
-        # rospy.loginfo_throttle(1, f"OBS Velocity  \t{robot_vel[0]:.2f}, {robot_vel[1]:.2f}")
-        # rospy.loginfo_throttle(1, f"OBS Distance: \t{goal_distance:.2f}")
-        # rospy.loginfo_throttle(1, f"OBS Heading:  \t{goal_bearing*180/math.pi:.2f}")
-
-        # Shift the buffer
-        self._obs_buffer[0,:-1, :] = self._obs_buffer[0,1:, :].clone()
-
-        # Define the values for each field
+        # Copy the computed values to the observations buffers
         # TODO: Removeh ardcoded values for the observations for testing
-        actions = np.array([[0.0, 0.0]], dtype=np.float32)  # 2
-        root_lin_vel_b = np.array([robot_vel], dtype=np.float32)  # 2
-        root_ang_vel_b = np.array([robot_ang_vel], dtype=np.float32)  # 1
-        goal_cos_sin = np.array([goal_cos_sin], dtype=np.float32)  # 2
-        distance = np.array([[goal_distance]], dtype=np.float32)  # 1
+        self.prev_actions[0] = [0.0, 0.0]
+        self.lin_vel[0] = robot_vel # 2
+        self.ang_vel[0] = robot_ang_vel  # 1
+        self.goal_cos_sin[0] = goal_cos_sin  # 2
+        self.goal_distance[0] = goal_distance  # 1
+
+
         observations = np.concatenate(
             [
-                actions,  # 2
-                root_lin_vel_b,  # 2
-                root_ang_vel_b,  # 1
-                goal_cos_sin,  # 1
-                distance,  # 1
+                self.prev_actions,  # 2
+                self.lin_vel,  # 2
+                self.ang_vel,  # 1
+                self.goal_cos_sin,  # 2
+                self.goal_distance,  # 1
             ],
             axis=1,
         )
         obs = dict({"obs": observations})
-
 
         return obs
                 
@@ -204,8 +174,6 @@ class RLAgent:
         if rospy.Time.now() - self.odom.header.stamp > self.odom_timeout:
             rospy.logwarn_throttle(1, "Odometry is too old...")
             return False
-        
-        # TODO: Check if autonomous mode is enabled
         
         # Check if goal has been reached
         if self.goal_reached():
@@ -271,7 +239,7 @@ class RLAgent:
 
         # Get actions from the policy
         tl, tr = self.get_action(obs)
-        self.previous_actions = [tl, tr]
+        self.prev_actions[0] = [tl, tr]
         # Publish the actions
         self.publish_cmds(tl, tr)
         self.rl_status_pub_.publish(Bool(True))
